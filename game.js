3378c77fe2928007ad9edd7c745a32b4cebba667
David <czdavid93@gmail.com>
2022-07-02 17:51:47 +0200

    created drawSnake to represent snake on the gameboard

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 let start, totalTimeElapsed, previousTimeStamp;
 let gameOver = false;
-const snakeSpeed = 2;
+const snakeSpeed = 1;
 
 function gameFlow(currentTime) {
   if (start === undefined) {
@@ -17,8 +17,10 @@ function gameFlow(currentTime) {
 
   if (secondsSinceLastRender < 1 / snakeSpeed) return;
 
-  console.log('render game board');
+  //console.log('render game board');
   previousTimeStamp = currentTime;
+
+  drawSnake();
 }
 
 window.requestAnimationFrame(gameFlow);
@@ -27,3 +29,27 @@ window.requestAnimationFrame(gameFlow);
 
 // DOCUMENTATION i used here:
 // https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+
+const gameBoard = document.getElementById('game-board-grid');
+
+const snakeBody = [
+  { x: 1, y: 1 },
+  { x: 2, y: 1 },
+  { x: 3, y: 1 },
+  { x: 4, y: 1 },
+  { x: 5, y: 1 },
+];
+
+function drawSnake() {
+  snakeBody.forEach((segment) => {
+    const snakeSegment = document.createElement('div');
+    snakeSegment.style.gridRowStart = segment.x;
+    snakeSegment.style.gridColumnStart = segment.y;
+    snakeSegment.classList.add('snake');
+    gameBoard.append(snakeSegment);
+    console.log('drawing snake');
+    console.log(segment);
+  });
+}
+
+// NOTES: i grabbed the gameBoard from DOM, created the snakeBody as an array of objects, each object represents one coordinate of a snake segment. the drawSnake() takes each segment and created a div on the gameBoard, places it where they have to be and adds class for styling.
